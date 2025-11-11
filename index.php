8ae4346bc9d7232a6276f1300783954c26e3ab03
keyvan pishevar <kpisheva@redhat.com>
2025-11-11 14:05:04 -0600

    Uncomment image output headers and readfile

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ $imagePath = 'images/test2.jpg';
 // Check if the file exists
 if (file_exists($imagePath)) {
     // Set the content type header
-    //header('Content-Type: image/jpeg');
+    header('Content-Type: image/jpeg');
 
     // Output the image file
-    //readfile($imagePath);
+    readfile($imagePath);
     echo 'ver 2.';
 } else {
     // Handle the case where the image does not exist
